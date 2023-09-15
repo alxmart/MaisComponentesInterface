3b91988c2bd7564d9e2090c4110c5ea2165daffe
alxmart <alxmartinez@gmail.com>
2023-09-15 12:32:54 -0300

    Created Landscape Qualifier.

--- app/src/main/java/com/luizafmartinez/maiscomponentesinterface/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/maiscomponentesinterface/MainActivity.kt
@@ -2,10 +2,23 @@ package com.luizafmartinez.maiscomponentesinterface
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnClique: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnClique = findViewById(R.id.btn_clique)
+        
+        btnClique.setOnClickListener {
+            Toast.makeText(this, "Olá", Toast.LENGTH_SHORT).show()
+        }
+
+        
     }
 }
