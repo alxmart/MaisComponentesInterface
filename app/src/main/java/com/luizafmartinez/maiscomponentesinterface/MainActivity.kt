b7efd1f34ce088fcf5dc2d729910243a23950cbf
alxmart <alxmartinez@gmail.com>
2023-09-15 16:53:43 -0300

    Configuring viewBinding.

--- app/src/main/java/com/luizafmartinez/maiscomponentesinterface/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/maiscomponentesinterface/MainActivity.kt
@@ -4,20 +4,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.luizafmartinez.maiscomponentesinterface.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnClique: Button
+    //private lateinit var btnClique: Button
+
+    //private lateinit var binding: ActivityMainBinding
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
-        btnClique = findViewById(R.id.btn_clique)
-        
+        //binding = ActivityMainBinding.inflate( layoutInflater )
+        setContentView(binding.root)
+
+        with (binding) {
+
+            btnClique.setOnClickListener { }
+            btnExecutar?.setOnClickListener { }
+        }
+
+        /*btnClique = findViewById(R.id.btn_clique)
         btnClique.setOnClickListener {
             Toast.makeText(this, "Olá", Toast.LENGTH_SHORT).show()
-        }
+        }*/
 
         
     }
