6716174016ee41c5801bcebb10f231ce4bf648f7
alxmart <alxmartinez@gmail.com>
2023-09-17 13:03:29 -0300

    Code to change group's visibility

--- app/src/main/java/com/luizafmartinez/maiscomponentesinterface/FloatingActionButtonActivity.kt
+++ app/src/main/java/com/luizafmartinez/maiscomponentesinterface/FloatingActionButtonActivity.kt
@@ -2,10 +2,29 @@ package com.luizafmartinez.maiscomponentesinterface
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import com.luizafmartinez.maiscomponentesinterface.databinding.ActivityFloatingActionButtonBinding
 
 class FloatingActionButtonActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityFloatingActionButtonBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_floating_action_button)
+        setContentView(binding.root)
+
+        with(binding) {
+            fabBotao.setOnClickListener {
+
+                groupMenu.visibility = if (groupMenu.isVisible) {
+                    View.INVISIBLE
+                } else {
+                    View.VISIBLE
+                }
+            }
+        }
     }
 }
