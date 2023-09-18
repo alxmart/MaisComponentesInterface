2ae6d045d263f8c1a56b5487903857d5ce66987e
alxmart <alxmartinez@gmail.com>
2023-09-18 18:36:08 -0300

    Added setOnClickListener for BUTTON "ENVIAR"

--- app/src/main/java/com/luizafmartinez/maiscomponentesinterface/FormularioActivity.kt
+++ app/src/main/java/com/luizafmartinez/maiscomponentesinterface/FormularioActivity.kt
@@ -2,10 +2,28 @@ package com.luizafmartinez.maiscomponentesinterface
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.luizafmartinez.maiscomponentesinterface.databinding.ActivityFormularioBinding
 
 class FormularioActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityFormularioBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_formulario)
+        setContentView(binding.root)
+
+        with (binding) {
+
+            btnEnviar.setOnClickListener {
+                checkbox()
+            }
+        }
+
+    }
+    private fun checkbox() {
+        val selecionado = binding.cbConfirmacao.isChecked
+        binding.textResultado.text = "valor selecionado: $selecionado"
     }
 }
