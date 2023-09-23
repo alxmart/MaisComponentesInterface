7b0c56320a7f3fb76e872ca6cfd4e3b25567ad4d
alxmart <alxmartinez@gmail.com>
2023-09-23 15:42:04 -0300

    Created new activity ToolbarActionbarActivity.kt and set to MAIN

---
+++ app/src/main/java/com/luizafmartinez/maiscomponentesinterface/ToolbarActionbarActivity.kt
@@ -0,0 +1,11 @@
+package com.luizafmartinez.maiscomponentesinterface
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ToolbarActionbarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toolbar_actionbar)
+    }
+}
