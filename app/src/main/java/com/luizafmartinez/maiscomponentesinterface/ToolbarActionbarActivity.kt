14f69b7ce50781b2d4ed943979d5ed6e97a17c6d
alxmart <alxmartinez@gmail.com>
2023-09-24 12:30:10 -0300

    Toolbar Customization

--- app/src/main/java/com/luizafmartinez/maiscomponentesinterface/ToolbarActionbarActivity.kt
+++ app/src/main/java/com/luizafmartinez/maiscomponentesinterface/ToolbarActionbarActivity.kt
@@ -18,10 +18,9 @@ class ToolbarActionbarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView( binding.root )
+        setContentView(binding.root)
 
         inicializarToolbar()
-
 
         //supportActionBar?.hide()
         //inicializarActionBar()
@@ -33,33 +32,67 @@ class ToolbarActionbarActivity : AppCompatActivity() {
         binding.tbPrincipal.setTitleTextColor(
             ContextCompat.getColor(this, R.color.white)
         )
+
+      /*  binding.tbPrincipal.inflateMenu(R.menu.menu_principal)
+        binding.tbPrincipal.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.item_adicionar -> {
+                    Toast.makeText(applicationContext, "Adicionar", Toast.LENGTH_SHORT).show()
+                    return@setOnMenuItemClickListener true
+                }
+
+                R.id.item_pesquisar -> {
+                    Toast.makeText(applicationContext, "Pesquisar", Toast.LENGTH_SHORT).show()
+                    return@setOnMenuItemClickListener true
+                }
+
+                R.id.item_configuracoes -> {
+                    Toast.makeText(applicationContext, "Configurações", Toast.LENGTH_SHORT).show()
+                    return@setOnMenuItemClickListener true
+                }
+
+                R.id.item_sair -> {
+                    Toast.makeText(applicationContext, "Sair", Toast.LENGTH_SHORT).show()
+                    return@setOnMenuItemClickListener true
+                }
+
+                else -> {
+                    true
+                }
+            }
+        }*/
+
         //binding.tbPrincipal.subtitle = "Mais detalhes"
+
         setSupportActionBar( binding.tbPrincipal )
+        inicializarActionBar()
     }
 
 
     private fun inicializarActionBar() {
 
         addMenuProvider(
-            object : MenuProvider{
+            object : MenuProvider {
                 override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
                     menuInflater.inflate(R.menu.menu_principal, menu)
                 }
 
                 override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
 
-                    when ( menuItem.itemId ) {
-
+                    when (menuItem.itemId) {
                         R.id.item_adicionar -> {
-                            Toast.makeText(applicationContext, "Adicionar", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(applicationContext, "Adicionar", Toast.LENGTH_SHORT)
+                                .show()
                         }
 
                         R.id.item_pesquisar -> {
-                            Toast.makeText(applicationContext, "Pesquisar", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(applicationContext, "Pesquisar", Toast.LENGTH_SHORT)
+                                .show()
                         }
 
                         R.id.item_configuracoes -> {
-                            Toast.makeText(applicationContext, "Configurações", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(applicationContext, "Configurações", Toast.LENGTH_SHORT)
+                                .show()
                         }
 
                         R.id.item_sair -> {
